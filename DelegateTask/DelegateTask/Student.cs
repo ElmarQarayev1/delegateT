4d7d7048e00e6667871f10cc2a676a8b018e6afe
ElmarQarayev1 <153679330+ElmarQarayev1@users.noreply.github.com>
2024-02-03 21:11:14 +0400

    ilaveler edildi

--- DelegateTask/DelegateTask/Student.cs
+++ DelegateTask/DelegateTask/Student.cs
@@ -41,10 +41,6 @@ namespace DelegateTask
 
             return totalPoint / examPoint.Count;
         }
-        public override string ToString()
-        {
-			return $"{FullName}-{No}-{examPoint}";
-        }
     }
 
 }
